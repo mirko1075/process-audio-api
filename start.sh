bb871b1e8d8687be62b7adea275a538362abc6ad
Mirko Siddi <mirko.siddi@gmail.com>
2025-01-22 00:12:19 +0100

    Update start.sh to remove log-level parameter from gunicorn command

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Start the application
-gunicorn app:app --bind 0.0.0.0:$PORT  --timeout 300 --workers 2 --log-level debug
+gunicorn app:app --bind 0.0.0.0:$PORT  --timeout 300 --workers 2
