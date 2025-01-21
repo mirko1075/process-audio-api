f088bca6667268109fc17d0fea4bdf527dec67f8
Mirko Siddi <mirko.siddi@gmail.com>
2025-01-21 21:57:15 +0100

    Update start.sh to enhance application startup configuration: Added timeout, increased worker count, and set log level for gunicorn.

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 # Install FFmpeg
 apt-get update && apt-get install -y ffmpeg
 # Start the application
-gunicorn app:app --bind 0.0.0.0:$PORT
+gunicorn app:app --bind 0.0.0.0:$PORT  --timeout 120 --workers 2 --log-level debug
